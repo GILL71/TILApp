02ddded5d4ac7e937d45e8bdbfae2f96ca113cc5
Michael Nechaev <gm360@mail.ru>
2019-01-30 17:16:52 +0300

    initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -100,33 +100,6 @@ public func routes(_ router: Router) throws {
         return .ok
     }
     
-//    router.post("api", "sendMail") {
-//        req in//-> Future<HTTPStatus> in
-//        let smtp = SMTP(
-//            hostname: "smtp.gmail.com",     // SMTP server address
-//            email: "gm360s@gmail.com",        // username to login
-//            password: "09127438gm95S"            // password to login
-//        )
-//
-//        let commail = Mail.User(name: "Michael Nechaev", email: "gm360s@gmail.com")
-//        let rumail = Mail.User(name: "Mikhail Nechaev", email: "gm360@mail.ru")
-//
-//        let mail = Mail(
-//            from: commail,
-//            to: [rumail],
-//            subject: "Humans and robots living together in harmony and equality.",
-//            text: "That was my ultimate wish."
-//        )
-//
-//        smtp.send(mail) { (error) in
-//            return "asd"
-//            if let error = error {
-//                print(error)
-//            }
-//        }
-//        return ".ok"
-//    }
-    
     router.post("api", "create", use: sessionManager.createTrackingSession)
     
     router.post("close", TrackingSession.parameter) {
